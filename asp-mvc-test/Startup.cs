8eea2b16c6bc3e2c96b652d8cb586a5ac6932193
Кавин Борис <kavinbob@gmail.com>
2015-09-29 20:35:21 +0300

    first commit

---
+++ asp-mvc-test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp_mvc_test.Startup))]
+namespace asp_mvc_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
