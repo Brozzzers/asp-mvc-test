0e4b8c38a6da8a7240942ccc103dbab1727ba401
Кавин Борис <kavinbob@gmail.com>
2015-09-29 20:42:19 +0300

    15-09-29-20-42

---
+++ asp-mvc-test/Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_mvc_test.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public string  hello()
+        {
+            return "Hello world";
+        }
+    }
+}
