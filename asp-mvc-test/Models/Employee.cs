11ed5e2ca14552bee1abb698eeb72592c392ca5b
kavinbob <kavinbob@gmail.com>
2015-10-01 14:40:23 +0300

    error

--- asp-mvc-test/Models/Employee.cs
+++ asp-mvc-test/Models/Employee.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace asp_mvc_test.Models
 {
     public class Employee
     {
+        [Key]
+        public string   EmployeeId  { get; set; }
         public string   FirstName   { get; set; }
         public string   LastName    { get; set; }
         public int      Salary      { get; set; }
--- asp-mvc-test/Models/EmployeeBusinessLayer.cs
+++ asp-mvc-test/Models/EmployeeBusinessLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using asp_mvc_test.DataAccessLayer;
 
 namespace asp_mvc_test.Models
 {
@@ -11,15 +12,9 @@ namespace asp_mvc_test.Models
         {
             List<Employee> le = new List<Employee>();
 
-            le.Add(new Employee("Boris", "Kavin", 35000));
-            le.Add(new Employee("Dari", "Kavin", 30000));
-            le.Add(new Employee("ignat", "Kavin", 5000));
-            le.Add(new Employee("Georgiy", "Kavin", 6000));
-            le.Add(new Employee("Bis", "Kavin", 40000));
-            le.Add(new Employee("Boris", "Kavin", 3000));
-            le.Add(new Employee("Boris", "Kavin", 35000));
+            SalesERPDAL salesDAL = new SalesERPDAL();
 
-            return le;
+            return salesDAL.Employees.ToList();
         }
     }
 }
