758713a2cf052dc5e7befd77427156aee96a7917
Кавин Борис <kavinbob@gmail.com>
2015-10-03 22:55:48 +0300

    end

--- asp-mvc-test/Models/Employee.cs
+++ asp-mvc-test/Models/Employee.cs
@@ -10,13 +10,33 @@ namespace asp_mvc_test.Models
     {
         [Key]      
         public int   EmployeeId  { get; set; }
+
         [Required(ErrorMessage ="Enter the First Name")]
         public string   FirstName   { get; set; }
+
         [Required(ErrorMessage = "Enter the Last Name")]
         [StringLength(5,ErrorMessage ="LastName should be not greater than 5")]
         public string   LastName    { get; set; }
+
+        [SalaryValidation]
         public int      Salary      { get; set; }
 
+    }
+
+    public class SalaryValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null)
+            {
+                return new ValidationResult("Enter the salary!");
+            }
+            if ((int)value > 50000)
+            {
+                return new ValidationResult("Too big!");
+            }
+            return ValidationResult.Success;
+        }
     }
 
 }
